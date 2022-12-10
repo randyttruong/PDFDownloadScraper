d6b4b29e386246c81a99b0c6d62440504eb4f2d3
randyrocher <randy.truong1113@gmail.com>
2022-12-10 00:30:29 -0600

    added test code for a single case

--- scrape.py
+++ scrape.py
@@ -1,5 +1,5 @@
 """
-Powerpoint Scraper 
+Powerpoint and PDF Scraper 
 Author: Randy Truong 
 Date: 15 November 2022
 
@@ -7,35 +7,69 @@ Description:
 This program is just made to take different webpages and scrape all of their hyperlinks,
 and then with the hyperlinks, download their contents.
 
-This program is mainly just a resource for me to download all of the slides/worksheets
-for different CS courses online. 
+This program is mainly just a resource for me to download all of the slides/worksheets for different CS courses online. 
 """
 
 from bs4 import BeautifulSoup as bs
+import requests as rq
 from urllib.request import (
-	urlopen, urlparse, urlunpause, urlretrieve) 
+    urlopen, urlparse, urlretrieve) 
 import os 
 import sys 
-import re 
-from treelib import Node, Tree  
+import wget 
+
+# Retrieve Command-Line Arguments 
+print(f"The passed in arguments are: {sys.argv}. There are a total of {len(sys.argv)} arguments") 
+
+sysArgs = sys.argv[1:]
+    
+headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Max-Age': '3600',
+    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
+    }
+
+def pretty_folders(): 
+    current_directory = os.listdir()
+    print("The current folders in your directory are:")
+    for each in current_directory: 
+        if os.path.isdir(each): 
+            print(each) 
+
 
 def get_all_links(soup):
     ''' 
     Parameters-- 
     soup: soup object of the current link 
     '''
-    
     urls = []
     all_urls = soup.find_all("a")
     for url in all_urls:
-        if "href" in url.attrs and url.attrs["href"] == "pdf":
+        if "href" in url.attrs and ".pdf" in url.attrs["href"]: 
             urls.append(url.attrs["href"])
     return urls
 
-class WebPage:
-	def __init__(self, page_directory, page_id): 
-		self.page_id = page_id 
-		self.WebPageSoup = soup()
-        self.links = get_all_links(self.WebPageSoup)
-
+def download_links(url, directory=(f"{os.getcwd()}/downloads")): 
+    page_id = "https://www.cs.cmu.edu/~charlie/courses/17-214/2021-spring/"
+    req = rq.get(url, headers) 
+    soup = bs(req.content, 'html.parser')
+    links = get_all_links(soup)
+    user_in = input(f"Would you like to download all {len(links)}? [Y/n] ")
+    if user_in.lower() == "y": 
+        for link in links: 
+            final_url = (page_id + link)
+            print(f"Getting {final_url}")
+            wget.download(page_id + link, out=directory)
+    elif user_in.lower() == "n":
+        None 
+    else:
+        print("Please try again.") 
+        download_links(url)
+print(os.getcwd())
+pretty_folders()
+for url in sysArgs: 
+    download_links(url)
+    
 
